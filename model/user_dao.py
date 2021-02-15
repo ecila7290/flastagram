3b9bd1e7cc422a16d7dcefd4c03976f228f83cd7
jeehoon kim <jihoon.kim89@gmail.com>
2021-02-15 18:23:46 +0900

    Add: added user signup and login api

--- model/user_dao.py
+++ model/user_dao.py
@@ -1,4 +1,47 @@
-
+from sqlalchemy import text
 
 class UserDao:
-    pass
+    def __init__(self, database):
+        self.db=database
+
+    def get_user(self, user_id):
+        user=self.db.execute(text("""
+            SELECT
+                id,
+                name,
+                profile
+            FROM users
+            WHERE id=:user_id
+        """),{'user_id':user_id}).fetchone()
+
+        return {
+            'id':user['id'],
+            'name':user['name'],
+            'profile':user['profile'],
+        } if user else None
+
+    def insert_user(self, user):
+        return self.db.execute(text("""
+            INSERT INTO users (
+                name,
+                email,
+                hashed_password,
+                profile
+            ) VALUES (
+                :name,
+                :email,
+                :password,
+                :profile
+            )
+        """),user).lastrowid
+
+    def get_user_id_and_password(self, email):
+        row=self.db.execute(text("""
+            SELECT
+                id,
+                hashed_password
+            FROM users
+            WHERE email=:email
+        """),{'email':email}).fetchone()
+        
+        return row if row else None
--- view/__init__.py
+++ view/__init__.py
@@ -1,4 +1,26 @@
-
+from flask import Flask, jsonify, request, current_app
 
 def create_endpoints(app, services):
-    pass
+    user_service=services.user_service
+
+    @app.route('/signup', methods=['POST'])
+    def signup():
+        new_user=request.json
+        new_user_id=user_service.create_new_user(new_user)
+        new_user=user_service.get_user(new_user_id)
+
+        return jsonify(new_user)
+
+    @app.route('/login', methods=['POST'])
+    def login():
+        credential=request.json
+        authorized=user_service.login(credential)
+
+        if authorized:
+            user_credential=user_service.get_user_id_and_password(credential['email'])
+            user_id=user_credential['id']
+            token=user_service.generate_access_token(user_id)
+
+            return jsonify({'user_id':user_id, 'access_token':token})
+        else:
+            return '', 401
